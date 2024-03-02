78198096471f1b0ced0c775fcdc7b37b10eb3905
Matheus Kikumori Almeida Leite <matheuskiku@gmail.com>
2024-03-02 22:36:00 +1000

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware.js')
 const app = express()
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-app.use(cors({credentials: true, origin: ["https://mern-blog-client-vrn5.onrender.com"], methods: ["POST", "GET"]}))
+app.use(cors({credentials: true, origin: ["https://mern-blog-client-vrn5.onrender.com"], methods: ["POST", "GET", "PATCH", "DELETE"]}))
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
