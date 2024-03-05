7518e3009c224be5de065b72431a6699e6476bc7
Matheus Kikumori Almeida Leite <matheuskiku@gmail.com>
2024-03-05 19:34:19 +1000

    modified:   server/controllers/postControllers.js

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -24,7 +24,7 @@ const createPost = async (req, res, next) => {
         let fileName = thumbnail.name
         let splittedFileName = fileName.split('.')
         let newFileName = splittedFileName[0] + uuid() + "." + splittedFileName[splittedFileName.length - 1]
-        async (err) => {
+        test = async (err) => {
             if(err) {
                 return next(new HttpError(err))
             } else {
